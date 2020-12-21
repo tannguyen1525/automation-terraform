25f19f47a4719b9e8943ea466c836b86e925a7d4
tannguyen1525 <minhtanit1990@gmail.com>
2020-12-21 12:27:44 +0700

    change code terraform 11

--- main.tf
+++ main.tf
@@ -74,6 +74,30 @@ resource "aws_route_table_association" "main-public-1-a"{
     route_table_id = aws_route_table.main-public.id
 }
 
+resource "aws_security_group" "allow-ssh" {
+    vpc_id = [aws_vpc.main.id]
+    name = "allow-ssh"
+    description = "security group that allows ssh and all egress traffic"
+    egress  {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress  {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "allow-ssh"
+    }
+    
+}
+
 # EC2 instance
 resource "aws_instance" "example"{
     ami             = var.ami
