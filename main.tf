c43beceb40c48e663aefd8c6e7bebbea72f197e7
tannguyen1525 <minhtanit1990@gmail.com>
2020-12-21 13:47:56 +0700

    change code terraform 13

--- main.tf
+++ main.tf
@@ -119,7 +119,7 @@ resource "aws_instance" "example"{
     subnet_id = aws_subnet.main-public-1.id
 
     # the security grup
-    vpc_security_group_ids = aws_security_group.allow-ssh.id
+    vpc_security_group_ids = [aws_security_group.allow-ssh.id]
 
     # the public SSH key
     #key_name = "${aws_key_pair.mykeypair.key_name}"
