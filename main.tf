262d316a930e65880343820816c364c1700496d7
tannguyen1525 <minhtanit1990@gmail.com>
2020-12-21 11:26:21 +0700

    change code terraform 9

--- main.tf
+++ main.tf
@@ -14,28 +14,89 @@ provider "aws"  {
   
 }
 
-# Create EC2 instance
-resource "aws_instance" "default" {
-    ami = "$(var.ami)"
-    //count = "$(var.count)"
-    key_name = "$(var.key_name)"
-    vpc_security_group_ids = [ "$(aws_security_group_rule.ingress_rules.id)" ]
-    source_dest_check = false
-    instance_type = "$(var.instance_type)"
-
+resource "aws_vpc" "main"{
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+    enable_dns_support = "true"
+    enable_dns_hostnames = "true"
+    enable_classiclink = "false"
     tags = {
-        Name = "EC2-linux"
+        Name = "VPC-main"
     }
-  
 }
 
-# Create Security Group for EC2
-resource "aws_security_group_rule" "ingress_rules" {
-  count = length(var.sg_ingress_rules)
-  type              = "ingress"
-  from_port         = var.sg_ingress_rules[count.index].from_port
-  to_port           = var.sg_ingress_rules[count.index].to_port
-  protocol          = var.sg_ingress_rules[count.index].protocol
-  cidr_blocks       = [var.sg_ingress_rules[count.index].cidr_block]
-    
+# Subnet
+resource "aws_subnet" "main-public-1"{
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = "true"
+    availability_zone = "us-east-1a"
+    tags = {
+        Name = "subnet_public"
+    }
+}
+
+resource "aws_subnet" "main-private-1"{
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.0.2.0/24"
+    map_public_ip_on_launch = "false"
+    availability_zone = "us-east-1b"
+    tags = {
+        Name = "subnet_private"
+    }
+}
+
+# Internet GW
+resource "aws_internet_gateway" "main-gw"{
+    vpc_id = "${aws_vpc.main.id}"
+    tags = {
+        Name = "IGW-main"
+    }
+}
+
+# Route tables
+resource "aws_route_table" "main-public" {
+    vpc_id ="${aws_vpc.main.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.main-gw.id}"
+       
+    }
+
+     tags = {
+            Name = "main-public-1"
+        }
+}
+
+# Route associations public
+resource "aws_route_table_association" "main-public-1-a"{
+    subnet_id = "${aws_subnet.main-public-1.id}"
+    route_table_id = "${aws_route_table.main-public.id}"
+}
+
+# EC2 instance
+resource "aws_instance" "example"{
+    ami             = "${var.ami}"
+    instance_type   = "t2.micro"
+    key_name = "Linux-instance"
+
+    tags = {
+        Name = "VM-test"
+    }
+
+    # create root block device (optional)
+    root_block_device {
+        volume_size = 10
+        volume_type = "gp2"
+        delete_on_termination = true # whether to delete root block device when the insance gets terminated or not
+    }
+
+    # the VPC subnet
+    subnet_id = "${aws_subnet.main-public-1.id}"
+
+    # the security group
+    vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
+
+    # the public SSH key
+    #key_name = "${aws_key_pair.mykeypair.key_name}"
 }
--- variables.tf
+++ variables.tf
@@ -14,28 +14,4 @@ variable "ami" {
   
 }
 
-variable "sg_ingress_rules" {
-    type = list(object({
-      from_port   = number
-      to_port     = number
-      protocol    = string
-      cidr_block  = string
-      description = string
-    }))
-    default     = [
-        {
-          from_port   = 22
-          to_port     = 22
-          protocol    = "tcp"
-          cidr_block  = "0.0.0.0/0"
-          description = "test"
-        },
-        {
-          from_port   = 80
-          to_port     = 80
-          protocol    = "tcp"
-          cidr_block  = "0.0.0.0/0"
-          description = "test"
-        },
-    ]
-}
+
