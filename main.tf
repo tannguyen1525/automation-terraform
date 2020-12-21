2bef3ac522347579c2da5d1f26d1eb652a470fec
tannguyen1525 <minhtanit1990@gmail.com>
2020-12-21 11:43:18 +0700

    change code terraform 10

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "aws_internet_gateway" "main-gw"{
 
 # Route tables
 resource "aws_route_table" "main-public" {
-    vpc_id ="${aws_vpc.main.id}"
+    vpc_id = "${aws_vpc.main.id}"
     route {
         cidr_block = "0.0.0.0/0"
         gateway_id = "${aws_internet_gateway.main-gw.id}"
@@ -95,7 +95,7 @@ resource "aws_instance" "example"{
     subnet_id = "${aws_subnet.main-public-1.id}"
 
     # the security group
-    vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
+    vpc_security_group_ids = "${aws_security_group.allow-ssh.id}"
 
     # the public SSH key
     #key_name = "${aws_key_pair.mykeypair.key_name}"
