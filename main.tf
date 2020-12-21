f0a6cf302ac853c5310ef7ac55aa1559b644f513
tannguyen1525 <minhtanit1990@gmail.com>
2020-12-21 11:06:07 +0700

    change code terraform 8

--- main.tf
+++ main.tf
@@ -31,12 +31,11 @@ resource "aws_instance" "default" {
 
 # Create Security Group for EC2
 resource "aws_security_group_rule" "ingress_rules" {
-  count = length(var.ingress_rules)
+  count = length(var.sg_ingress_rules)
   type              = "ingress"
-  from_port         = var.ingress_rules[count.index].from_port
-  to_port           = var.ingress_rules[count.index].to_port
-  protocol          = var.ingress_rules[count.index].protocol
-  cidr_blocks       = [var.ingress_rules[count.index].cidr_block]
-  description       = var.ingress_rules[count.index].description
-  security_group_id = aws_security_group.ec2_security_groups.id
+  from_port         = var.sg_ingress_rules[count.index].from_port
+  to_port           = var.sg_ingress_rules[count.index].to_port
+  protocol          = var.sg_ingress_rules[count.index].protocol
+  cidr_blocks       = [var.sg_ingress_rules[count.index].cidr_block]
+    
 }
