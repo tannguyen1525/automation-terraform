50acfe90c937acca3e518083e65916e203803ff9
tannguyen1525 <minhtanit1990@gmail.com>
2020-12-21 15:15:43 +0700

    change code terraform 14

--- main.tf
+++ main.tf
@@ -93,6 +93,13 @@ resource "aws_security_group" "allow-ssh" {
     }
 
     ingress  {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress  {
         from_port = 80
         to_port = 80
         protocol = "tcp"
