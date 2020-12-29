550fa0e131e0cf0259e57b2653581cd970ed7c0c
tannguyen1525 <minhtanit1990@gmail.com>
2020-12-29 15:25:20 +0700

    change code main

--- main.tf
+++ main.tf
@@ -81,14 +81,7 @@ resource "aws_security_group" "allow-ssh" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-
-    ingress  {
-        from_port = 443
-        to_port = 443
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
+    
     ingress  {
         from_port = 80
         to_port = 80
