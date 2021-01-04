cabc1f18d03ae482d0b7819521546d129d3e3502
tannguyen1525 <45476201+tannguyen1525@users.noreply.github.com>
2021-01-04 13:45:30 +0700

    Delete backend-s3.tf

--- backend-s3.tf
+++
@@ -1,9 +0,0 @@
-# Terraform state will be store in S3
-terraform {
-  backend "s3" {
-      bucket = "terraform-bucket-tannm"
-      key = "terraform.tfstate"
-      encrypt = "true"
-      region = "us-east-1"
-  }
-}
